dddb4a8d41a0740346e5b7d17e0ab338938448b0
Tal Jacob <taljacob2@gmail.com>
2023-03-20 01:18:24 +0200

    Remove Redundant Code In `mongo-init.js`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- mongo-init.js
+++ mongo-init.js
@@ -22,8 +22,6 @@ function getNextSequence(name) {
    return ret.seq;
 }
 
-db.items.createIndex({ "item.zip": 1 }, { unique: false });
-
 db.items.insert({ _id: getNextSequence("itemId"), "name": "apples", "qty": 5, "rating": 3 });
 db.items.insert({ _id: getNextSequence("itemId"), "name": "bananas", "qty": 7, "rating": 1, "microsieverts": 0.1 });
 db.items.insert({ _id: getNextSequence("itemId"), "name": "oranges", "qty": 6, "rating": 2 });
