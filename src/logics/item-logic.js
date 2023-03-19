09a9eb42a2a0d515d126f3ca21d533fc63353c60
Tal Jacob <taljacob2@gmail.com>
2023-03-19 23:22:57 +0200

    Remove comment
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/logics/item-logic.js
+++ src/logics/item-logic.js
@@ -1,7 +1,6 @@
 const mongodb = require("mongodb");
 const dal = require("../data-access-layer/dal");
 
-// Get all products: 
 function getAllItemsAsync() {
     return new Promise((resolve, reject) => {
         dal.getDatabase().collection("items").find({}).toArray((err, items) => {
