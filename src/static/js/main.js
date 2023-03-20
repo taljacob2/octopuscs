3d3859426f546180acfe8106b6499b229a172f3b
Tal Jacob <taljacob2@gmail.com>
2023-03-20 14:25:15 +0200

    Backup
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,18 @@
-(function ($) {
-    "use strict";
-    console.clear();
-}(JQuery));
+// (function ($) {
+//     "use strict";
+//     console.clear();
+
+//     function fetchApples() {
+//         $.get("http://localhost:3000/api/items",
+//             function (data, status) {
+//                 $('#apples-quantity').text(data)
+//             }
+//         );
+//     }
+
+//     console.log("hello")
+
+//     fetchApples()
+// }(JQuery));
+
+console.log("hello2")
